2b5e59311c47b73138dc636279d2e12dbf021260
aakash447447 <aakashsharma@jklu.edu.in>
2021-01-03 01:25:31 +0530

    about page and nodemailer

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const express = require('express')
-const app = express()
-const userRouter = require('./server/routes/routes')
-const path = require('path')
+const express = require("express");
+const app = express();
+const userRouter = require("./server/routes/routes");
+const path = require("path");
+const nodemailer = require('nodemailer')
+const bodyParser = require('body-parser')
 
-app.use(express.static(path.join(__dirname, '/public')))
-app.set('views', path.join(__dirname, 'views'));
 
-console.log('dirname:',__dirname)
+app.use(express.static(path.join(__dirname, "/public")));
+app.set("views", path.join(__dirname, "views"));
+
+console.log("dirname:", __dirname);
 app.set("view engine", "ejs");
+//middleware
+app.use(bodyParser.urlencoded({extended: true}))
 
 //Routes
-app.get('/',(req,res)=>{
-    console.log(' in app.get ')
-    res.render('User/index')
-})
-console.log('process.env.NODE_ENV:',process.env.NODE_ENV)
 
-    app.listen(()=>{
-        console.log(`Server is started`)
-    })
+app.use('/',userRouter)
 
 
+
+// console.log("process.env.NODE_ENV:", process.env.NODE_ENV);
+
+app.listen(() => {
+  console.log(`Server is started`);
+});
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,49 @@
-const express = require('express')
-const router  = express.Router()
+const express = require("express");
+const router = express.Router();
+const nodemailer = require('nodemailer')
 
-router.get('/user',(req,res)=>{
-    console.log('in user route')
-    res.render('User/index')
-})
+router.get("/about", (req, res) => {
+  console.log(" in app.get  /about ");
+  res.render("User/About/about");
+});
 
-module.exports = router 
+// POST route from contact form
+router.post("/contact", (req, res) => {
+    console.log('in contact route:',req.body)
+  // Instantiate the SMTP server
+  const smtpTrans = nodemailer.createTransport({
+    host: "in3.fcomet.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'aakashsharma@globewick.com',
+      pass: 'aakash@98',
+    },
+  });
+
+  // Specify what the email will look like
+  const mailOpts = {
+    from: req.body.email, // This is ignored by Gmail
+    to: "aakashsharma@globewick.com",
+    subject: req.body.subject,
+    text: `name: ${req.body.name},\nemail:${req.body.email} ,\n\nmessage:   ${req.body.message}`,
+  };
+
+  // Attempt to send the email
+  smtpTrans.sendMail(mailOpts, (error, response) => {
+    if (error) {
+        res.json({
+            message:"Email failed "
+        }) // Show a page indicating failure
+    } else {
+        res.render("User/Home/index"); // Show a page indicating success
+    }
+  });
+});
+
+router.get("/", (req, res) => {
+  console.log(" in app.get / ");
+  res.render("User/Home/index");
+});
+
+module.exports = router;
